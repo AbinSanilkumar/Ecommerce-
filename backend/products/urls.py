eb3812f1f2c0921ba148f3378451f8154e356936
Abin <abi903719@gmail.com>
2026-02-28 09:48:49 +0530

    Upgrade ProductSerializer with nested category and absolute image URL

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from . import views
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet, CategoryViewSet
 
-urlpatterns = [
-    path('', views.product_list, name='product-list'),
-]
+router = DefaultRouter()
+router.register(r'products', ProductViewSet, basename='product')
+router.register(r'categories', CategoryViewSet, basename='category')
+
+urlpatterns = router.urls
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,10 +1,34 @@
-from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from .models import Product
-from .serializers import ProductSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
 
-@api_view(['GET'])
-def product_list(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+
+    filterset_fields = ['price', 'stock']
+    search_fields = ['name', 'description']
+    ordering_fields = ['price', 'created_at']
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = 'slug'
+
+    @action(detail=True, methods=['get'])
+    def products(self, request, slug=None):
+        category = self.get_object()
+        products = category.products.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
