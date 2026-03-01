999724d723dd22d3098f2ed452fc9b9b1a824a1d
Abin <abi903719@gmail.com>
2026-03-01 09:51:07 +0530

    Add order status, payment simulation, and admin control

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, CategoryViewSet, CartViewSet
+from .views import ProductViewSet, CategoryViewSet, CartViewSet, OrderViewSet
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
 router.register(r'categories', CategoryViewSet, basename='category')
 router.register(r'cart', CartViewSet, basename='cart')
+router.register(r'orders', OrderViewSet, basename='orders')
 
 urlpatterns = router.urls
